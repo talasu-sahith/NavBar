94e86cd4995786d7144da4e79c9fa47b078fb68e
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-18 23:24:56 +0530

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links } from "./data";
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const handleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="" />
+          <button onClick={handleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul>
+              {links.map((link) => {
+                const { url, text, id } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
